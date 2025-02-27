2949c91c2130301603043d3cbbdd459ad2dd92ed
Jefferson Sarti <jeffersonsarti@gmail.com>
2025-02-27 14:27:30 -0300

    chore(issue/2): Layout inicial & configurações de tema

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    primary: '#0070f3',
+    secondary: '#1c1c1c',
+    background: '#f5f5f5',
+    text: '#333333',
+    error: '#ff0000',
+  },
+  fonts: {
+    body: "'Inter', sans-serif",
+  },
+};
